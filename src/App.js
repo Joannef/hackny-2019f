404b8885997674e57a54dcb5fa924673f44b8aea
Joanne <joannef3188@gmail.com>
2019-09-28 23:06:39 -0400

    add branch and result.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Webcam from 'react-webcam';
+import Result from 'result.js';
 import './App.css';
 
 const WebcamComponent = () => <Webcam />;
@@ -9,8 +10,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
-          <Webcam />
-          <button>Capture photo</button>
+        <Webcam />
+        <button>Capture photo</button>
+      </div>
+      <div>
+        <Result />
       </div>
     );
   }
